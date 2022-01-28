d8607f9206e8ed923271c44e85cec1d6b7b23f9d
MrCloud-10 <yusufblt10@outlook.com>
2022-01-28 21:45:24 +0300

    +

--- src/app/models/user/user.datasource.ts
+++ src/app/models/user/user.datasource.ts
@@ -6,7 +6,7 @@ export class UserDataSource{
     constructor(){
       this.users=new Array<User>(
         new User(1,"Yusuf","Bulut","user.png","yusufbulut","yusufblt10@outlook.com","12345abC"),
-        new User(2,"Otokoç","Otomotiv","user.png","otokoç","otokoç@gmail.com","Otokoç123"),
+        new User(2,"Otokoç","Otomotiv","user.png","otokoç","otokoc@otokoc.com","Otokoç123"),
         new User(3,"Java S.","Angular","user.png","jsangular","jsangular@npm.com","npmInsta11"),
         new User(4,"a","b","user.png","abcd","abc@abc.com","1234asdF")
       );
